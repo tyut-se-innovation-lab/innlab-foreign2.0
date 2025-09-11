941cac6ea7c98a9d394eb960ac8a27d366968661
wxycherry <163128074+wxycherry@users.noreply.github.com>
2025-09-11 12:11:53 +0800

    fix:首页23级展示+个人列表返回页修复

--- innlab-web/innlab-back-ui/src/utils/authorizationHttp.js
+++ innlab-web/innlab-back-ui/src/utils/authorizationHttp.js
@@ -3,7 +3,8 @@ import axios from 'axios'; // 改为直接从 axios 库导入
 
 const authorizationHttp = axios.create({
   // baseURL: 'http://49.232.217.248:8087',
-  baseURL: 'http://localhost:8081',
+  // baseURL: 'http://localhost:8081',
+  baseURL: 'http://47.94.132.207:18081',
   timeout: 10000,
 });
 
